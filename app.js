f3d732d94bd5b0f25b0eca0bee74e961f9005185
Cuong <bdcuong91@gmail.com>
2020-06-09 16:47:19 +0200

    adding comments

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-app.set('view engine','ejs');
+//fuer MONGODB, aber diese App hat kein DBS => brauchst nicht
+//const mongoose = require('mongoose');
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => {
-    res.render('index');
-  });
+  res.render('index');
+});
 
-// app.listen(process.env.PORT);
+//Heroku 
+app.listen(process.env.PORT);
 
+//localhost:3000
 app.listen(3000);
-
--- public/control/control.js
+++ public/control/control.js
@@ -1,12 +1,12 @@
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 var lastDice;
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    if(gamePlaying) {
+//fuer wuerfeln funktion
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamePlaying) {
         // 1. Random number
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
@@ -14,6 +14,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //2. Display the result
         document.getElementById('dice-1').style.display = 'block';
         document.getElementById('dice-2').style.display = 'block';
+        //Pfad 
         document.getElementById('dice-1').src = '/image/dice-' + dice1 + '.png';
         document.getElementById('dice-2').src = '/image/dice-' + dice2 + '.png';
 
@@ -26,31 +27,33 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             //Next player
             nextPlayer();
         }
-        
-        
-    }    
+
+
+    }
 });
 
 
-document.querySelector('.btn-hold').addEventListener('click', function() {
+document.querySelector('.btn-hold').addEventListener('click', function () {
     if (gamePlaying) {
         // Add CURRENT score to GLOBAL score
         scores[activePlayer] += roundScore;
+        //scores[0] = gesamtpunkte fuer player 1
 
         // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-        
+
+        //Input score 
         var input = document.querySelector('.final-score').value;
         var winningScore;
-        
-        // Undefined, 0, null or "" are COERCED to false
-        // Anything else is COERCED to true
-        if(input) {
+
+
+        if (input) {
             winningScore = input;
         } else {
+            //default Score
             winningScore = 100;
         }
-        
+
         // Check if player won the game
         if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
@@ -87,23 +90,34 @@ function nextPlayer() {
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
+    //total Punkte fuer jeden Spieler
     scores = [0, 0];
+    //Flage = 0 spieler-1, 1 = spieler-2
     activePlayer = 0;
+    //Aktuell
     roundScore = 0;
+    //Flage ob spielt oder nicht
     gamePlaying = true;
 
+    //nicht Wuerfeln zeigen - 
     document.getElementById('dice-1').style.display = 'none';
     document.getElementById('dice-2').style.display = 'none';
 
+    //Ausgangssituation
+    //erster Gesamtpunkt
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
+    //
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
+    //
     document.getElementById('name-0').textContent = 'Spieler 1';
     document.getElementById('name-1').textContent = 'Spieler 2';
+    //Remove class .winner, .active 
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
+    //Der Spieler 1 darf zuerst wuerfeln
     document.querySelector('.player-0-panel').classList.add('active');
 }
